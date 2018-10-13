c7a27d4ea77ef2b3ca5c2513622c067092140ce3
Cedric van Putten <me@bycedric.com>
2018-10-14 00:12:27 +0200

    feature: add primary app page

---
+++ src/pages/app/elements.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+/**
+ * The app container wraps the content and centers it both horizontally and vertically.
+ */
+export const AppContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+`;
