5e37846adbbdada1417616a3fbb02013ffa9fe29
Cedric van Putten <me@bycedric.com>
2018-10-19 21:53:23 +0200

    refactor: move user description highlights to github user organism
    This removes all references of GitHub from the user molecule.

--- src/organisms/github-user/github-user.js
+++ src/organisms/github-user/github-user.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AsyncUser } from 'src/providers/github';
 import User from 'src/molecules/user';
+import highlights from './highlights';
 
 export default function GithubUserOrganism() {
 	return (
@@ -12,6 +13,7 @@ export default function GithubUserOrganism() {
 						username={data.login}
 						avatarUrl={data.avatar_url}
 						description={data.bio}
+						highlights={highlights}
 					/>
 				)}
 			</AsyncUser.Resolved>
