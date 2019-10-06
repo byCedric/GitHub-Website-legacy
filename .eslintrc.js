5d63127ad86ae59a34168003fc585d1161997758
Cedric van Putten <me@bycedric.com>
2019-10-06 12:48:01 +0000

    chore: add linting script with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+		'react-hooks',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	env: {
+		'node': true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'react/no-unescaped-entities': 'off',
+		'react/prop-types': 'off',
+	},
+};
